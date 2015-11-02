ed16490408602a43a8864f8f8a2165910839bb4d
Pierre A Pirault <pierre.pirault@outlook.com>
2015-11-02 06:40:39 -0500

    asynchronously run the majority of the script.

--- runrun.sh
+++ runrun.sh
@@ -3,10 +3,10 @@ exists(){
 }
 
 sudo apt-get update -y
-sudo apt-get upgrade -y
+sudo apt-get upgrade -y &
 
 if ! exists git; then
-  sudo apt-get install -y git
+  sudo apt-get install -y git &
 fi
 
 if ! exists node; then
@@ -18,15 +18,15 @@ if ! exists nvm; then
 fi
 
 if ! exists vim; then
-  sudo apt-get install -y vim
+  sudo apt-get install -y vim &
 fi
 
-nvm install stable
+nvm install stable &
 
-sudo npm install -g npm
+sudo npm install -g npm &
 
-cp ./.vimrc ~/
+cp ./.vimrc ~/ &
 git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim
-vim +PluginInstall +qall
+vim +PluginInstall +qall &
 
 git config --global core.editor "vim"
