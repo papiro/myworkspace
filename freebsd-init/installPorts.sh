5363709000b282a12017bf64204a232a2bccd741
papiro <pierre.pirault@outlook.com>
2017-01-05 18:57:48 -0500

    wrong order of execution

--- freebsd-init/installPorts.sh
+++ freebsd-init/installPorts.sh
@@ -4,6 +4,12 @@ installPort () {
   cd $1 && make install clean BATCH=yes  
 }
 
+# Get ports collection
+echo "portsnap fetch"
+portsnap fetch
+echo "portsnap extract"
+portsnap extract
+
 # Install tools
 echo "installing subversion"
 installPort /usr/ports/devel/subversion
@@ -13,10 +19,4 @@ echo "installing node"
 installPort /usr/ports/www/node
 echo "installing rkhunter"
 installPort /usr/ports/security/rkhunter
-
-# Get ports collection
-echo "portsnap fetch"
-portsnap fetch
-echo "portsnap extract"
-portsnap extract
 
