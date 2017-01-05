87046437ed7820c0536a34f26ec8012d8be0747d
papiro <pierre.pirault@outlook.com>
2017-01-05 18:55:01 -0500

    split out port install in order to run modularly when needed

---
+++ freebsd-init/installPorts.sh
@@ -0,0 +1,22 @@
+#!/bin/sh
+
+installPort () {
+  cd $1 && make install clean BATCH=yes  
+}
+
+# Install tools
+echo "installing subversion"
+installPort /usr/ports/devel/subversion
+echo "installing git"
+installPort /usr/ports/devel/git
+echo "installing node"
+installPort /usr/ports/www/node
+echo "installing rkhunter"
+installPort /usr/ports/security/rkhunter
+
+# Get ports collection
+echo "portsnap fetch"
+portsnap fetch
+echo "portsnap extract"
+portsnap extract
+
--- freebsd-init/runrun2.sh
+++ freebsd-init/runrun2.sh
@@ -1,9 +1,5 @@
 #!/bin/sh
 
-installPort () {
-  cd $1 && make install clean BATCH=yes  
-}
-
 add2rc () {
   echo $1 >> /etc/rc.conf
 }
@@ -30,26 +26,14 @@ echo "tweak /etc/ssh/sshd_config"
 echo "Add AllowUsers entry..."
 vim /etc/ssh/sshd_config
 
-# Get ports collection
-echo "portsnap fetch"
-portsnap fetch
-echo "portsnap extract"
-portsnap extract
+# install Ports
+`installPorts.sh`
 
 # Install Kernel sources
-echo "installing subversion"
-installPort /usr/ports/devel/subversion
 echo "installing kernel sources"
 echo "svn checkout https://svn0.us-west.freebsd.org/base/releng/10.2/ /usr/src"
 svn checkout https://svn0.us-west.freebsd.org/base/releng/10.2/ /usr/src
 
-# Install tools
-echo "installing git"
-installPort /usr/ports/devel/git
-echo "installing node"
-installPort /usr/ports/www/node
-echo "installing rkhunter"
-installPort /usr/ports/security/rkhunter
 echo "add rkhunter stuff to periodic.conf..."
 read foo
 vim /etc/defaults/periodic.conf
